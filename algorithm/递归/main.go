c3c41bfa4617f16cee604933b2ac9666067e28f2
chenli694 <chenli25@jd.com>
2021-05-26 11:40:09 +0800

    add binarysearch code

--- algorithm/递归/main.go
+++ algorithm/递归/main.go
@@ -1,4 +1,4 @@
-//斐波那契数列  阶乘
+//斐波那契数列  阶乘  二分查找
 package main
 
 import "fmt"
@@ -15,6 +15,21 @@ func main() {
 	//阶乘：N!=N*(N-1)*(N-2)......
 	fmt.Println("factorial1 value is ", factorial1(n))
 	fmt.Println("factorial2 value is ", factorial2(n, 1))
+
+	array := []int{1, 3, 7, 9, 16, 21, 110, 243, 999}
+	target := 21
+	result := binarysearch(array, target, 0, len(array)-1)
+	fmt.Println(target, result)
+	target = 1
+	result = binarysearch(array, target, 0, len(array)-1)
+	fmt.Println(target, result)
+
+	target1 := 22
+	result1 := binarysearch(array, target1, 0, len(array)-1)
+	fmt.Println(target1, result1)
+	target1 = 999
+	result1 = binarysearch(array, target1, 0, len(array)-1)
+	fmt.Println(target1, result1)
 
 }
 
@@ -44,4 +59,52 @@ func factorial2(n int, acc int) int {
 		return acc
 	}
 	return factorial2(n-1, acc*n)
+}
+
+//二分查找递归方法
+func binarysearch(array []int, target int, l, r int) int {
+	if l > r {
+		//l -> left, r -> rigth, 如果l > r, 则越界了
+		return -1
+	}
+
+	// 从中间开始找
+	mid := (l + r) / 2 //数字常量除法，除了移位运算，如果二元运算的操作数是不同种类的无类型常量，运算结果使用以下种类靠后的一个:整数、unicode字符、浮点数、复数，示例中除法运算两个常量都是整数常量，运算结果因此也是一个整数常量
+
+	middleNum := array[mid]
+	if middleNum == target {
+		//找到了
+		return mid
+	} else if middleNum < target {
+		//中间数字比目标数字小，从右边找
+		return binarysearch(array, target, mid+1, r)
+	} else {
+		//中间数字比目标数字大，从左边找
+		return binarysearch(array, target, l, mid-1)
+	}
+}
+
+//二分查找非递归方法
+func binarysearch2(array []int, target int, l, r int) int {
+	ltemp := l
+	rtemp := r
+
+	for {
+		if ltemp > rtemp {
+			return -1 //越界了
+		}
+
+		//从中间开始找
+		mid := (ltemp + rtemp) / 2
+		middleNum := array[mid]
+
+		if middleNum == target {
+			return mid //找到了
+		} else if middleNum > target {
+			rtemp = mid - 1 //中间的数比目标数还大，从左边找
+		} else {
+			ltemp = mid + 1 //中间的数比目标数还小，从右边找
+		}
+	}
+
 }
---
+++ function_study/main.go
@@ -0,0 +1,2 @@
+//golang 方法是值接收者还是指针接收者的问题
+package main
